6f226e447e5bfe6ee4dd022999b26ec087d88fdb
aidant <aidant@udel.edu>
2022-03-16 19:56:19 -0400

    loaded in the json file properly

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Quiz } from "../interfaces/quiz";
 import { Question, QuestionType } from "../interfaces/question";
 
@@ -7,10 +7,31 @@ import sample from "../data/quizzes.json";
 const QUIZZES = sample.map(
     (quiz): Quiz => ({
         ...quiz,
-        questionList: quiz.questionList.map((q): Question => ({...q, submission: "", type: (QuestionType)q.type})),
+        questionList: quiz.questionList.map(
+            (q): Question => ({
+                ...q,
+                submission: "",
+                type: q.type as QuestionType
+            })
+        )
     })
 );
 
+/* {quizzes.map((quiz: Quiz) => (
+                <div key={quiz.id}>
+                    {quiz.id + " " + quiz.questionList[0].body}
+                </div>
+            ))} */
+
 export function Quizzer(): JSX.Element {
-    return <h3>Quizzer</h3>;
+    const [quizzes, setQuizzes] = useState<Quiz[]>(QUIZZES);
+    return (
+        <>
+            {quizzes.map((quiz: Quiz) => (
+                <div key={quiz.id}>
+                    {quiz.id + " " + quiz.questionList[0].body}
+                </div>
+            ))}
+        </>
+    );
 }
