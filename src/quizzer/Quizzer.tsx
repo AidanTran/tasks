34673db5240271f432f1d1c1661bede5c94f0834
aidant <aidant@udel.edu>
2022-03-16 19:33:39 -0400

    organised json file and am trying at loadin git ni

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,4 +1,15 @@
 import React from "react";
+import { Quiz } from "../interfaces/quiz";
+import { Question, QuestionType } from "../interfaces/question";
+
+import sample from "../data/quizzes.json";
+
+const QUIZZES = sample.map(
+    (quiz): Quiz => ({
+        ...quiz,
+        questionList: quiz.questionList.map((q): Question => ({...q, submission: "", type: (QuestionType)q.type})),
+    })
+);
 
 export function Quizzer(): JSX.Element {
     return <h3>Quizzer</h3>;
