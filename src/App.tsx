210bfaf5dba9a893a1833377ef6037ac59f659bb
aidant <aidant@udel.edu>
2022-03-16 17:55:44 -0400

    added minimal navbar and switching functionality between tasks and quizzer

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { Container, Col, Row, Button } from "react-bootstrap";
-import { ChangeType } from "./components/ChangeType";
-import { RevealAnswer } from "./components/RevealAnswer";
-import { StartAttempt } from "./components/StartAttempt";
-import { TwoDice } from "./components/TwoDice";
-import { CycleHoliday } from "./components/CycleHoliday";
-import { Counter } from "./components/Counter";
-import { DoubleHalf } from "./bad-components/DoubleHalf";
-import { ColoredBox } from "./bad-components/ColoredBox";
-import { ShoveBox } from "./bad-components/ShoveBox";
-import { ChooseTeam } from "./bad-components/ChooseTeam";
-import { CheckAnswer } from "./form-components/CheckAnswer";
-import { GiveAttempts } from "./form-components/GiveAttempts";
-import { EditMode } from "./form-components/EditMode";
-import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
-import { ChangeColor } from "./form-components/ChangeColor";
 import { Quizzer } from "./quizzer/Quizzer";
+import { Button } from "react-bootstrap";
+import { ShowHideTasks } from "./ShowHideTasks";
 
 function App(): JSX.Element {
+    const [quizzer, setQuizzer] = useState<boolean>(true);
+
     return (
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
+                <Button
+                    onClick={() => {
+                        setQuizzer(!quizzer);
+                    }}
+                >
+                    {quizzer ? "Quizzer" : "Tasks"}
+                </Button>
             </header>
-<<<<<<< HEAD
-            <section className="section">
-                <h1>Aidan Tran</h1>
-                <div className="orangutan__img">
-                    <img
-                        src={require("./orangutan.png")}
-                        alt="picture of orangutan"
-                    />
-                </div>
-                <Container>
-                    <Row>
-                        <Col>
-                            <ul>
-                                <li>Orangutan</li>
-                                <li>Orangutan</li>
-                                <li>Orangutan</li>
-                            </ul>
-                            <div className="red_rectangle"></div>
-                        </Col>
-                        <Col>
-                            <Button
-                                onClick={() => {
-                                    console.log("Hello World!");
-                                }}
-                            >
-                                Log Hello World
-                            </Button>
-                            <div className="red_rectangle"></div>
-                        </Col>
-                    </Row>
-                </Container>
-                <p>
-                    Edit <code>src/App.tsx</code> and save. This page will
-                    automatically reload. Hello World!
-                </p>
-            </section>
-=======
-            <Quizzer></Quizzer>
->>>>>>> upstream/task-quizzer
-            <hr></hr>
-            <CheckAnswer expectedAnswer="42"></CheckAnswer>
-            <hr></hr>
-            <GiveAttempts></GiveAttempts>
-            <hr></hr>
-            <EditMode></EditMode>
-            <hr></hr>
-            <ChangeColor></ChangeColor>
-            <hr></hr>
-            <MultipleChoiceQuestion
-                options={["a", "b", "c"]}
-                expectedAnswer="b"
-            ></MultipleChoiceQuestion>
-            <hr></hr>
-            <DoubleHalf></DoubleHalf>
-            <hr></hr>
-            <ChooseTeam></ChooseTeam>
-            <hr></hr>
-            <ColoredBox></ColoredBox>
-            <hr></hr>
-            <ShoveBox></ShoveBox>
-            <hr></hr>
-            <Counter></Counter>
-            <hr />
-            <RevealAnswer></RevealAnswer>
-            <hr />
-            <StartAttempt></StartAttempt>
-            <hr />
-            <TwoDice></TwoDice>
-            <hr />
-            <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
+            {quizzer && <Quizzer></Quizzer>}
+            <ShowHideTasks visible={!quizzer}></ShowHideTasks>
         </div>
     );
 }
