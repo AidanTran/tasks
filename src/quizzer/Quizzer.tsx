f79ef50769dd4c963e4a044931ba58c97c3e11a7
aidant <aidant@udel.edu>
2022-03-22 16:33:18 -0400

    added my sketch and completed features

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -61,6 +61,48 @@ export const Quizzer = () => {
                 handleClose={handleCloseModal}
                 addQuiz={addQuiz}
             ></AddQuizModal>
+            <hr />
+            <h2 style={{ color: "white" }}>Application Sketch</h2>
+            <img src={require("./sketchFINAL.jpg")} />
+            <hr />
+            <div style={{ color: "white" }}>
+                <h2>Completed Features</h2>
+                <ul className="completedList">
+                    <li>
+                        Users can see a list of quizzes, including the quizzes
+                        title, description, and how many questions it has
+                    </li>
+                    <li>
+                        Users can select a specific quiz to see the questions,
+                        including the questions name, body, and points
+                    </li>
+                    <li>
+                        Quiz questions can be of AT LEAST two types: a short
+                        answer question or multiple choice question
+                    </li>
+                    <li>
+                        Users can enter or choose an answer for a quiz question,
+                        and be told if they are correct
+                    </li>
+                    <li>
+                        Users can see how many total points they have earned
+                    </li>
+                    <li>
+                        Users can clear out their existing answers for a quiz
+                    </li>
+                    <li>Users can publish or unpublish a question</li>
+                    <li>
+                        Users can filter the questions in a list so that only
+                        published questions are shown
+                    </li>
+                    <li>Users can edit the questions and fields of a quiz</li>
+                    <li>Users can add a new quiz question</li>
+                    <li>Users can delete an existing quiz question</li>
+                    <li>Users can reorder quiz questions</li>
+                    <li>Users can add a new quiz</li>
+                    <li>Users can delete an existing quiz</li>
+                </ul>
+            </div>
         </div>
     );
 };
